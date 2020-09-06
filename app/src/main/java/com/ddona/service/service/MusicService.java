81182c2f1d54fdea38b1831d6dd3aa6efd0fea02
Trung Doan <doanptand@gmail.com>
2020-09-06 20:33:23 +0700

    add notification

--- app/src/main/java/com/ddona/service/service/MusicService.java
+++ app/src/main/java/com/ddona/service/service/MusicService.java
@@ -1,12 +1,17 @@
 package com.ddona.service.service;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+import com.ddona.service.R;
 import com.ddona.service.media.MediaManager;
 
 public class MusicService extends Service {
@@ -17,13 +22,33 @@ public class MusicService extends Service {
         super.onCreate();
         mediaManager = new MediaManager(this);
         mediaManager.play();
-        Log.d("doanpt","oncreate");
+        Log.d("doanpt", "oncreate");
+        runForeground();
+    }
+
+    private void runForeground() {
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setContentTitle("Music");
+        builder.setContentText("This is song name");
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel =
+                    new NotificationChannel("1",
+                            "music",
+                            NotificationManager.IMPORTANCE_HIGH);
+            NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            manager.createNotificationChannel(channel);
+            builder.setChannelId(channel.getId());
+        }
+
+        Notification notification = builder.build();
+        startForeground(1, notification);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d("doanpt","onStartCommand");
-        return START_STICKY;
+        Log.d("doanpt", "onStartCommand");
+        return START_REDELIVER_INTENT;
     }
 
     @Nullable
